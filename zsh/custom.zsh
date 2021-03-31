201b12b7747267ee06d885a5a5488444e3f44e63
Marcello Seri <marcello.seri@gmail.com>
2021-03-31 16:51:23 +0200

    Done
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -105,10 +105,8 @@ fi
 
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
-#### 
+#### Final Customizations ####
 
-
-####
 export TERM_ITALICS=true
 export CLICOLOR=true
 
