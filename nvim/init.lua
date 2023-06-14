afbbace29c6461a38665ca040df481cb43949244
Marcello Seri <marcello.seri@gmail.com>
2023-06-14 14:11:19 +0200

    Update
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,6 +46,24 @@ vim.opt.termguicolors = true
 
 vim.opt.diffopt = 'filler,internal,algorithm:histogram,indent-heuristic,followwrap'
 
+if vim.g.neovide then
+  vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
+
 require('plugins')
 
 vim.cmd [[
