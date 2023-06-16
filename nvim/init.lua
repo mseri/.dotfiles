e98946de7f51e84522092615c928295f4fceba46
Marcello Seri <marcello.seri@gmail.com>
2023-06-16 11:48:17 +0200

    Update neovide setup
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -63,7 +63,10 @@ vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true}
 vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 
-
+if vim.g.neovide then
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animation_length = 0
+end
 require('plugins')
 
 vim.cmd [[
