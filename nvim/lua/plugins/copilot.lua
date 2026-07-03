ef760c08d4cf9ca2c001f70d93aeabf2e87effe2
Marcello Seri <marcello.seri@gmail.com>
2026-07-03 09:02:55 +0200

    Remove copilot chat from nvim
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,34 +1,26 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        filetypes = {
-          markdown = true,
-          sh = function ()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '.*rc')
-              or string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '.*env.*')
-            then
-              -- disable for *rc or *env files
-              return false
-            end
-            return true
-          end,
-        },
-        suggestion = {
-          auto_trigger = true,
-        },
-      })
-    end,  
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or 
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                filetypes = {
+                    markdown = true,
+                    sh = function()
+                        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '.*rc')
+                            or string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '.*env.*')
+                        then
+                            -- disable for *rc or *env files
+                            return false
+                        end
+                        return true
+                    end,
+                },
+                suggestion = {
+                    auto_trigger = true,
+                },
+            })
+        end,
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
-  },
 }
